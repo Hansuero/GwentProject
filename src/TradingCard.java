bcb9a211575d23de6cd7c9107e57fb376f359979
tadaoni <112934438+tadaoni@users.noreply.github.com>
2022-12-23 20:35:35 +0800

    Add files via upload

--- src/TradingCard.java
+++ src/TradingCard.java
@@ -2,11 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
-public class TradingCard extends JLabel
-{
+public class TradingCard extends JLabel {
     // Members
-    public enum CardType
-    {MELEE, RANGED, MAGIC, DEBUG}
+    public enum CardType {MELEE, RANGED, MAGIC, DEBUG}
 
     private boolean hasAbility;
     ;
@@ -18,10 +16,9 @@ public class TradingCard extends JLabel
     // Methods
 
     /**
-     * Default constructor creates a useless card
+     * 这是Default constructor， 创建一张空白的无用卡
      */
-    TradingCard()
-    {
+    TradingCard() {
         super();
         this.isWeather = false;
         this.setSize(50, 60);
@@ -34,10 +31,13 @@ public class TradingCard extends JLabel
     }
 
     /**
-     * Create a new card if you have access to the CardType enum
+     * 已知卡牌类型的前提下对卡牌创建，卡牌类型用CardType类表示
+     *
+     * @param power 设想的点数
+     * @param name  名字
+     * @param type  卡牌类型
      */
-    TradingCard(int power, String name, CardType type)
-    {
+    TradingCard(int power, String name, CardType type) {
         super();
         this.isWeather = false;
         this.setSize(108, 200);
@@ -50,10 +50,13 @@ public class TradingCard extends JLabel
     }
 
     /**
-     * Create a new card using a String for its type
+     * 已知卡牌类型的前提下对卡牌创建，卡牌类型用String类型表示
+     *
+     * @param power 设想的点数
+     * @param name  名字
+     * @param type  卡牌类型
      */
-    TradingCard(int power, String name, String type)
-    {
+    TradingCard(int power, String name, String type) {
         super();
         this.isWeather = false;
         this.setSize(108, 200);
@@ -62,26 +65,23 @@ public class TradingCard extends JLabel
         this.setIcon(icon);
         this.power = power;
         this.name = name;
-        if (type.contains("MELEE"))
-        {
+        if (type.contains("MELEE")) {
             this.type = CardType.MELEE;
-        } else if (type.contains("RANGED"))
-        {
+        } else if (type.contains("RANGED")) {
             this.type = CardType.RANGED;
-        } else if (type.contains("MAGIC"))
-        {
+        } else if (type.contains("MAGIC")) {
             this.type = CardType.MAGIC;
-        } else
-        {
+        } else {
             this.type = CardType.DEBUG;
         }
     }
 
     /**
-     * Copy Constructor for TradingCard
+     * 对原有卡牌进行复制的构造
+     *
+     * @param card 要复制的牌
      */
-    TradingCard(TradingCard card)
-    {
+    TradingCard(TradingCard card) {
         super();
         this.isWeather = false;
         this.setSize(56, 85);
@@ -94,58 +94,58 @@ public class TradingCard extends JLabel
     }
 
     /**
-     * Accessor for power
+     * 获取卡牌点数
+     *
+     * @return 卡牌点数
      */
-    public int getPower()
-    {
+    public int getPower() {
         return power;
     }
 
     /**
-     * Accessor for name
+     * 获取卡牌名字
+     *
+     * @return 卡牌名字
      */
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
     /**
-     * Accessor for type
+     * 获取卡牌类型
+     *
+     * @return 卡牌类型
      */
-    public CardType getType()
-    {
+    public CardType getType() {
         return type;
     }
 
-    public void print()
-    {
+    /**
+     * 打印卡牌类型
+     */
+    public void print() {
         System.out.println(name + " " + type + " " + power);
     }
 
     /**
      * ID:[0]农民能力：复制对方已出卡牌中最高的点数
+     * 
+     * @param gameModel
      */
-    public void copyEnemyHighestPower(PlayModel gameModel)
-    {
+    public void copyEnemyHighestPower(PlayModel gameModel) {
         int highestPower = 2;
-        for (TradingCard card : gameModel.p2MagicBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p2MagicBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
-        for (TradingCard card : gameModel.p2MeleeBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p2MeleeBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
-        for (TradingCard card : gameModel.p2RangedBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p2RangedBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
@@ -158,28 +158,23 @@ public class TradingCard extends JLabel
 
     /**
      * ID:[0]农民能力：复制对方已出卡牌中最高的点数(AI)
+     *
+     * @param gameModel
      */
-    public void copyPlayerHighestPower(PlayModel gameModel)
-    {
+    public void copyPlayerHighestPower(PlayModel gameModel) {
         int highestPower = 2;
-        for (TradingCard card : gameModel.p1MagicBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p1MagicBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
-        for (TradingCard card : gameModel.p1MeleeBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p1MeleeBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
-        for (TradingCard card : gameModel.p1RangedBoard)
-        {
-            if (card.getPower() > highestPower)
-            {
+        for (TradingCard card : gameModel.p1RangedBoard) {
+            if (card.getPower() > highestPower) {
                 highestPower = card.getPower();
             }
         }
@@ -192,9 +187,11 @@ public class TradingCard extends JLabel
 
     /**
      * ID:[5]僵尸能力：
+     *
+     * @param gameModel
+     * @param gameWindow
      */
-    public void killEnemyRangedCard(PlayModel gameModel, PlayView gameWindow)
-    {
+    public void killEnemyRangedCard(PlayModel gameModel, PlayView gameWindow) {
         gameModel.p2RangedBoard.clear();
         gameWindow.p2Ranged.removeAll();
         gameWindow.p2Ranged.repaint();
@@ -202,9 +199,11 @@ public class TradingCard extends JLabel
 
     /**
      * ID:[5]僵尸能力：杀死对方ranged行所有的牌(AI)
+     *
+     * @param gameModel
+     * @param gameWindow
      */
-    public void killPlayerRangedCard(PlayModel gameModel, PlayView gameWindow)
-    {
+    public void killPlayerRangedCard(PlayModel gameModel, PlayView gameWindow) {
         gameModel.p1RangedBoard.clear();
         gameWindow.p1Ranged.removeAll();
         gameWindow.p1Ranged.repaint();
@@ -212,15 +211,15 @@ public class TradingCard extends JLabel
 
     /**
      * ID:[10]魔法学徒能力：己方每有一个magic，自身点数加倍
+     *
+     * @param gameModel
      */
-    public void doublePowerWhenHaveAMagic(PlayModel gameModel)
-    {
+    public void doublePowerWhenHaveAMagic(PlayModel gameModel) {
         int magicCount = 0;
-        for (TradingCard card : gameModel.p1MagicBoard)
-        {
+        for (TradingCard card : gameModel.p1MagicBoard) {
             magicCount++;
         }
-        int doubledPower = (int)Math.pow(2, magicCount + 1);
+        int doubledPower = (int) Math.pow(2, magicCount + 1);
         this.power = doubledPower;
         ImageIcon icon = new ImageIcon("res/image/" + name + power + ".jpg");
         icon = new ImageIcon(icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_DEFAULT));
@@ -228,16 +227,16 @@ public class TradingCard extends JLabel
     }
 
     /**
-     * ID:[10]魔法学徒能力：己方每有一个magic，自身点数加倍
+     * ID:[10]魔法学徒能力：己方每有一个magic，自身点数加倍(AI)
+     *
+     * @param gameModel
      */
-    public void doublePowerWhenHaveAMagicForAI(PlayModel gameModel)
-    {
+    public void doublePowerWhenHaveAMagicForAI(PlayModel gameModel) {
         int magicCount = 0;
-        for (TradingCard card : gameModel.p2MagicBoard)
-        {
+        for (TradingCard card : gameModel.p2MagicBoard) {
             magicCount++;
         }
-        int doubledPower = (int)Math.pow(2, magicCount + 1);
+        int doubledPower = (int) Math.pow(2, magicCount + 1);
         this.power = doubledPower;
         ImageIcon icon = new ImageIcon("res/image/" + name + power + ".jpg");
         icon = new ImageIcon(icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_DEFAULT));
--- src/WeatherCard.java
+++ src/WeatherCard.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Locale;
 
-public class WeatherCard extends JLabel
-{
+public class WeatherCard extends JLabel {
+    //Members
     private String weatherName;
-    public enum WeatherType
-    {
+
+    public enum WeatherType {
 
         CLEAR,
         ECLIPSE,
@@ -21,54 +21,60 @@ public class WeatherCard extends JLabel
     boolean isWeather;
 
     // Methods
-    WeatherCard(String type)
-    {
+
+    /**
+     * 已知天气类型的前提下对卡牌创建，卡牌类型用String类型表示
+     *
+     * @param type 卡牌类型
+     */
+    WeatherCard(String type) {
         super();
         this.isWeather = true;
         weatherName = type;
         this.setSize(108, 200);
-        ImageIcon icon = new ImageIcon("res/image/" + type.toLowerCase() +".jpg");
+        ImageIcon icon = new ImageIcon("res/image/" + type.toLowerCase() + ".jpg");
         icon = new ImageIcon(icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_DEFAULT));
         this.setIcon(icon);
-        if (type.contains("CLEAR"))
-        {
+        if (type.contains("CLEAR")) {
             this.Weathertype = WeatherType.CLEAR;
-        } else if (type.contains("ECLIPSE"))
-        {
+        } else if (type.contains("ECLIPSE")) {
             this.Weathertype = WeatherType.ECLIPSE;
-        } else if (type.contains("FOG"))
-        {
+        } else if (type.contains("FOG")) {
             this.Weathertype = WeatherType.FOG;
-        } else if (type.contains("HEATWAVE"))
-        {
+        } else if (type.contains("HEATWAVE")) {
             this.Weathertype = WeatherType.HEATWAVE;
-        } else if (type.contains("NICEBREEZE"))
-        {
+        } else if (type.contains("NICEBREEZE")) {
             this.Weathertype = WeatherType.NICEBREEZE;
-        } else if (type.contains("RAIN"))
-        {
+        } else if (type.contains("RAIN")) {
             this.Weathertype = WeatherType.RAIN;
-        } else if (type.contains("WIND"))
-        {
+        } else if (type.contains("WIND")) {
             this.Weathertype = WeatherType.WIND;
         }
 
     }
 
-    WeatherCard(WeatherCard card)
-    {
+    /**
+     * 通过复制另一张牌来创建卡牌
+     *
+     * @param card 要复制的卡牌
+     */
+    WeatherCard(WeatherCard card) {
         super();
         isWeather = true;
         weatherName = card.weatherName;
         this.setSize(50, 60);
-        ImageIcon icon = new ImageIcon("res/image/"+ weatherName +".jpg");
+        ImageIcon icon = new ImageIcon("res/image/" + weatherName + ".jpg");
         icon = new ImageIcon(icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_DEFAULT));
         this.setIcon(icon);
         this.Weathertype = card.Weathertype;
     }
 
-    public WeatherType getWeatherType()
-    {
+    /**
+     * 获取天气牌类型
+     *
+     * @return 天气类型
+     */
+    public WeatherType getWeatherType() {
         return this.Weathertype;
     }
 }
